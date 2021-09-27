7c97c3a6d05b2d2a873d4bcf9aa4b6f774aaa16a
Jorge Maset <jorgemaset@hotmail.com>
2021-09-27 18:39:15 +0200

    primera clase

--- pmydm/src/main5.java
+++ pmydm/src/main5.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class main5 {
+
+
     public static void main(String[] args) {
 
         //
@@ -17,26 +19,75 @@ public class main5 {
         marcas.add("Citroen");
         marcas.add("BMW");
 
-        List<String> Bingo = new ArrayList<>();
 
-        int ale= 0;
-        for(int i = 0; i < 5;)
+        List<String> Bingo2 = generarCarton (marcas);
+
+
+        //generar n cartones de tal manera que no este ninguno repetido( que no contengan las mismas 5 marcas)
+        // a b c d e SERIA EL MISMO que a b c e d
+       ArrayList<List<String>> contenedorCartones = new ArrayList<>();
+
+        int nCartones = 20;
+        for(int i = 0; i < nCartones; i++)
         {
-            ale = (int) (Math.random()*9);
-            if(!Bingo.contains(marcas.get(ale))){
-                Bingo.add(marcas.get(ale));
-                 i++;
+            List<String> cartonI = generarCarton(marcas);
+            if(!existeCarton(cartonI,contenedorCartones))
+            {
+                contenedorCartones.add(cartonI);
+                imprimirCarton(cartonI);
             }
+
         }
 
+    }
+
+    /**
+     * esta funcion devuelve si el carton introducido esta en el contenedor o no para añadirlo
+     * @param cartonI este es el carton que se va a comparar
+     * @param contenedorCartones estructura que contiene los cartones utilizados
+     * @return devuelve si el carton introducido esta en el contenedor o no para añadirlo
+     */
+    private static boolean existeCarton(List<String> cartonI, ArrayList<List<String>> contenedorCartones) {
+        for (int i = 0; i < contenedorCartones.size(); i++) {
+           if(comparar2Cartones(cartonI, contenedorCartones.get(i)))
+           {
+               return true;
+           }
+        }
+        return false;
+    }
+
+    private static boolean comparar2Cartones(List<String> cartonI, List<String> Carton2) {
+        //coges primer carton y vas viendo elemento por elemento si esta y si no esta ninguno return true
+        for (int i = 0; i < cartonI.size(); i++) {
+            if(!cartonI.contains(Carton2.get(i))) return false;
+
+        }
+         return true;
+    }
+
+    private static void imprimirCarton(List<String> CartonInt) {
         String sComma = "";
-        for(String carton:Bingo)
+        for(String carton: CartonInt)
         {
             System.out.print(sComma+carton);
             sComma=", ";
         }
+        System.out.println();
+    }
 
-
+    private static List<String> generarCarton(List<String> marcasInt) {
+        List<String> carton = new ArrayList<String>();
+        int ale= 0;
+        for(int i = 0; i < 5;)
+        {
+            ale = (int) (Math.random()*9);
+            if(!carton.contains(marcasInt.get(ale))){
+                carton.add(marcasInt.get(ale));
+                i++;
+            }
+        }
+        return carton;
     }
 
 }
